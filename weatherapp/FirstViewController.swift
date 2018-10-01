0addfa48e269efac1010fc4a250ae5251fe56a4b
Niko Mattila <c5nimatt@kc408mac08.tamk.ad>
2018-10-01 11:58:52 +0300

    Initial Commit

---
+++ weatherapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  weatherapp
+//
+//  Created by Niko Mattila on 01/10/2018.
+//  Copyright © 2018 Niko Mattila. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
